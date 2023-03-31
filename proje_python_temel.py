a0e96eb9be0fb4deb7c70d685b8771ca5ced213c
OsmanAliWaberi <osmanaliwaberi@gmail.com>
2023-04-01 00:58:08 +0300

    error çözdum

--- proje_python_temel.py
+++ proje_python_temel.py
@@ -6,17 +6,21 @@
 
 def flatten(liste):
     t=("[","]", ",","'")
-    string =""
-    for i in liste :
-        string += str(i)
+    string1 =""
+    string2 = ""
     new_string = ""
-    for i in s:
+    for i in liste :
+        string1 += str(i)
+    
+    for i in string1:
         if i in t :
             continue
         else:
-            string+= i
-    new_liste = string.split(" ") 
+            string2+= i
+    new_liste = string2.split(" ")
     return new_liste
+
+    
 def tersi(liste):
     liste=[]
     for i in l:
